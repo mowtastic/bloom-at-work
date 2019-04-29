e5b04371abaa152079a41fb1f903bb6f45850fc4
Pierre Lemée <pierre@pierrelemee.fr>
2019-04-29 09:17:42 +0200

    Add PHPunit

---
+++ tests/DumbTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BloomAtWork\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DumbTest extends TestCase
+{
+    public function testDumb()
+    {
+        $this->assertTrue(true, "If not true then false");
+    }
+}
